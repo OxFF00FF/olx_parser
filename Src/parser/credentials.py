87737e81a97f0d89190e5f96165ca809aa6822cb
OxFF00FF <Wot-98_98@mail.ru>
2025-06-25 10:56:35 +0600

    испрвил получение токена через браузер

--- Src/parser/credentials.py
+++ Src/parser/credentials.py
@@ -1,6 +1,5 @@
 import datetime
 import os
-import shutil
 import time
 
 import undetected_chromedriver as uc
@@ -30,7 +29,7 @@ def get_token(user_dir='guest', show_info=None) -> str | None:
     user_dir_existed = os.path.exists(user_dir)
 
     if not user_dir_existed:
-        print(f"\n‼️  {LIGHT_YELLOW}Папка пользователя не найдена. Сейчас откроется браузер, вам нужно войти в свой аккаунт OLX в течении минуты и дождаться когда браузер закроется{WHITE}")
+        print(f"\n‼️  {LIGHT_YELLOW}Папка пользователя не найдена{WHITE} · Сейчас откроется браузер, вам нужно войти в свой аккаунт OLX в течении минуты и дождаться когда браузер закроется")
         if os.path.exists(creds_file):
             os.remove(creds_file)
         time.sleep(10)
@@ -60,54 +59,63 @@ def get_token(user_dir='guest', show_info=None) -> str | None:
                 if show_info:
                     print(f"\n⌛️  Время действия токена: {LIGHT_MAGENTA}{formatted_time}{WHITE}")
                     return
+                return data.get('access_token')
 
-                return data.get('token')
             else:
                 print(f"\n⚠️  {YELLOW}Время действия токена истекло{WHITE} · Получаем новый")
                 time.sleep(2)
+
         else:
             print(f"\n⚠️  {YELLOW}Файл с токеном не найден{WHITE} · Получаем новый")
             time.sleep(2)
 
-        # Если файла нет илитоен истек, то создаем и запускаем драйвер
+        # Если файла нет или токен истек, то создаем и запускаем драйвер
         # logger.info(f"⚙️  {DARK_GRAY}[Profile: {user_dir}]{WHITE}")
 
         driver = uc.Chrome(options=chrome_options)
         driver.implicitly_wait(60)
 
-        driver.get('https://www.olx.ua/myaccount/')
+        driver.get('https://www.olx.ua/myaccount/settings')
 
+        # Если нет папки профиля то ждем минуту, чтобы успеть войти в аккаунт
         if not user_dir_existed:
             driver.maximize_window()
             time.sleep(60)
 
+        # Если нет файла с токеном то ждем 30 сек, чтобы успеть повторно войти
+        if not os.path.exists(creds_file):
+            time.sleep(30)
+
         driver.minimize_window()
         if driver.title == 'OLX.UA - Увійти':
-            logger.error(f"❌  {RED}Не удалось получить токен. Попробуйтн снова. Возможно сработала капча или вы не успели войти в аккаунт{WHITE}")
-            shutil.rmtree(user_dir)
-            exit()
+            print(f"❌  {LIGHT_RED}Не удалось получить токен{WHITE} · Попробуйтн снова (Возможно сработала капча или вы не успели войти в аккаунт)")
+            return None
         else:
-            logger.info(f"✔️  {driver.title}")
+            if 'satisfied' in driver.title:
+                print(f"Сработала блокировка CloudFront. Попробуйте позже")
+                return None
+            else:
+                print(f"✔️  {driver.title}")
 
         access_token = next((cookie['value'] for cookie in driver.get_cookies() if cookie['name'] == 'access_token'), None)
-        token = f"Bearer {access_token}"
 
         if not access_token:
             print(f"⚠️  Не удалось получить токен · {access_token=}")
             return None
 
-        seconds = 800
+        seconds = 600
         minutes = seconds // 60
         expire_date = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
         expire_ts = int(expire_date.timestamp())
         formatted_time = expire_date.strftime('%H:%M:%S')
 
-        time.sleep(3)
-        save_json(dict(token=token, timestamp=expire_ts, time=formatted_time, date=str(expire_date)), creds_file)
+        access_token = f"Bearer {access_token}"
+        save_json(dict(access_token=access_token, timestamp=expire_ts, time=formatted_time, date=str(expire_date)), creds_file)
         print(f"\n⌛️  {LIGHT_GREEN}Токен получен{WHITE} · Истекает через {LIGHT_YELLOW}{minutes} мин{WHITE} в {LIGHT_MAGENTA}{formatted_time}{WHITE}")
-        time.sleep(3)
+        return access_token
 
-        return token
+    except Exception as e:
+        logger.error(f"Ошибка во время работы драйвера · {e}")
 
     finally:
         if driver:
