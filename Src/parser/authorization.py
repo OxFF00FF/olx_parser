83d7b024ac2e4d02888e13a2c520c3fffa953c97
OxFF00FF <Wot-98_98@mail.ru>
2025-06-26 15:38:14 +0600

    fix

--- Src/parser/authorization.py
+++ Src/parser/authorization.py
@@ -76,7 +76,7 @@ def get_session_id(user_dir='guest') -> str | None:
         else:
             session_id = open_json(auth_file).get('login_sid')
             if session_id:
-                print(f"✔️  {LIGHT_GREEN}Текущий SID{WHITE} · {session_id}\n")
+                print(f"✔️  {LIGHT_GREEN}Текущий SID{WHITE} · {session_id}")
                 return session_id
             else:
                 print(f"❌  Не удалось найти идентификатор сесии в файле · {auth_file}\n")
